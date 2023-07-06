9bc82c44e1bffd930f931ca9c2e6abc9204758be
DaniilD3 <138785804+DaniilD3@users.noreply.github.com>
2023-07-06 17:50:42 +0300

    Add files via upload

--- game/createField.js
+++ game/createField.js
@@ -1,5 +1,35 @@
+import { createMenu } from "./createMenu.js";
+
 export const createField =(lvl)=>{
+	const main = document.querySelector("main");
+	main.classList.add ("null");
+
+
 	console.log(lvl);
+	let lvlText;
+	
+	switch(lvl){
+		case 8:
+			lvlText ="Easy"
+			break;
+		case 12 :
+			lvlText ="Medium"
+			break;
+		case 16:
+			lvlText ="Hard"
+			break;
+	}
+
+	const game = document.querySelector(".game");
+	game.style.display = "block"
+			const level = game.querySelector(".lvl")
+		level.textContent = lvlText;
+
+		const menu = 	game.querySelector(".btn");
+		menu.addEventListener('click',()=>{
+			createMenu();
+		})
+
 
 
 
--- game/createMenu.js
+++ game/createMenu.js
@@ -1,8 +1,12 @@
 import { createField } from "./createField.js";
 
 export const createMenu=()=>{
+	const main = document.querySelector("main");
+	main.classList.remove ("null");
+	const game = document .querySelector(".game");
 	const menu = document .querySelector(".menu");
 		menu.style.display = "block";
+			game.style.display = "none"
 	
 	const btn = menu .querySelectorAll(".btn")
 		btn.forEach (el => {
@@ -10,12 +14,15 @@ export const createMenu=()=>{
 		console.log(el.textContent);
 		switch(el.textContent){
 			case"Easy":
+			menu.style.display="none";
 			createField(8);
 				break;
 			case"Medium":
+			menu.style.display="none";
 			createField(12);
 				break;
 			case"Hard":
+			menu.style.display="none";
 			createField(16);
 				break;
 		}
